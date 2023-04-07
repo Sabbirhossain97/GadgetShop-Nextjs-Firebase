0d2090ff09a97334df2bf696134e74322084eb52
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2023-04-07 22:30:57 +0600

    fixed another conditional rendering issue to check length of cart in /Cart page

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -62,7 +62,6 @@ export default function Cart() {
 
   useEffect(() => {
     setItems(state.items);
-    
   }, [state]);
 
   useEffect(() => {
@@ -78,7 +77,11 @@ export default function Cart() {
       <Navbar />
       <div className="min-h-screen bg-gray-100 p-20 ">
         <h1 className="mt-20 mb-10 text-center text-2xl font-bold">
-          {items ? items.length === 0 ? "Your Cart is Empty!" : "Shopping Cart" : ""}
+          {items
+            ? items.length === 0
+              ? "Your Cart is Empty!"
+              : "Shopping Cart"
+            : ""}
         </h1>
         <div className="mt-20 mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
@@ -163,42 +166,46 @@ export default function Cart() {
                 ))
               : ""}
           </div>
-          {items.length > 0 ? (
-            <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-              <div className="mb-2 flex justify-between">
-                <p className="text-gray-700">Subtotal</p>
-                <p className="text-gray-700">
-                  ${subtotal ? subtotal.toFixed(2) : ""}
-                </p>
-              </div>
-              <div className="flex justify-between">
-                <p className="text-gray-700">Shipping</p>
-                <p className="text-gray-700">Free</p>
-              </div>
-              <hr className="my-4" />
-              <div className="flex justify-between">
-                <p className="text-lg font-bold">Total</p>
-                <div className="">
-                  <p className="mb-1 text-lg font-bold ml-10">
-                    $
-                    {items.reduce(
-                      (acm, currentElm) =>
-                        acm + currentElm.price * currentElm.quantity,
-                      0
-                    )}
+          {items ? (
+            items.length > 0 ? (
+              <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+                <div className="mb-2 flex justify-between">
+                  <p className="text-gray-700">Subtotal</p>
+                  <p className="text-gray-700">
+                    ${subtotal ? subtotal.toFixed(2) : ""}
                   </p>
-                  <p className="text-sm text-gray-700 ml-4">including VAT</p>
                 </div>
+                <div className="flex justify-between">
+                  <p className="text-gray-700">Shipping</p>
+                  <p className="text-gray-700">Free</p>
+                </div>
+                <hr className="my-4" />
+                <div className="flex justify-between">
+                  <p className="text-lg font-bold">Total</p>
+                  <div className="">
+                    <p className="mb-1 text-lg font-bold ml-10">
+                      $
+                      {items.reduce(
+                        (acm, currentElm) =>
+                          acm + currentElm.price * currentElm.quantity,
+                        0
+                      )}
+                    </p>
+                    <p className="text-sm text-gray-700 ml-4">including VAT</p>
+                  </div>
+                </div>
+
+                <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+                  <Link href="/Checkout">Check out</Link>
+                </button>
+
+                <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+                  <Link href="/">Continue Shopping</Link>
+                </button>
               </div>
-
-              <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
-                <Link href="/Checkout">Check out</Link>
-              </button>
-
-              <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
-                <Link href="/">Continue Shopping</Link>
-              </button>
-            </div>
+            ) : (
+              ""
+            )
           ) : (
             ""
           )}
