183628301c15667f94387ec38252f13146e39a02
Sabbirhossain97 <sabbirhossainbd199@gmail.com>
2024-06-11 13:30:58 +0600

    refactored

--- services/firebase.js
+++ services/firebase.js
@@ -27,10 +27,10 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "nextjs-ecommerce-c1457.appspot.com",
-  messagingSenderId: "395338672751",
-  appId: "1:395338672751:web:eff1ed764cfaf8f087fb9a",
-  measurementId: "G-Z4FMQ0EL41",
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGIN_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
